35e6014f9c230705520c4a74b40ee72d338e38af
kilas-aniso <kilasaniso@gmail.com>
2025-03-08 15:24:32 -0500

    added the model and url for resource app

--- studybud_backend/resources/views.py
+++ studybud_backend/resources/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Resource
+from .serializers import ResourceSerializer
 
-# Create your views here.
+class ResourceListCreateView(generics.ListCreateAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+
+class ResourceDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer

e3ca150252c89fe3d48ec3eed9c3cef56c51e286
kilas-aniso <kilasaniso@gmail.com>
2025-03-09 00:30:18 -0500

    created user and studybuddy apps, initialised their models and api url configuration

--- studybud_backend/backend/urls.py
+++ studybud_backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("resources.urls")),  # Include API routes
+    path('admin/', admin.site.urls),
+    path('api/users/', include('user.urls')),
+    path('api/studybuddy/', include('studybuddy.urls')),
 ]
